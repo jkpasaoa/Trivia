3eaf209481983013593b6c2d87cffd43e20fa62d
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-01-19 18:43:27 -0500

    2 css. 2 Js. 2 html.

---
+++ main.js
@@ -0,0 +1,21 @@
+//error if input a number instead of a name
+function submitCheck(e) {
+    e.preventDefault();
+    const nameInput = document.querySelector("#playerNameInput").value;
+    const isNumber = !isNaN(nameInput);
+    const isEmpty = nameInput.length === 0;
+    if (isNumber || isEmpty) {
+        alert("That is not a valid input. Please type again!");
+        return false;
+    } else {
+        const playerNameInput = String(document.getElementById("playerNameInput").value);
+        console.log(playerNameInput)
+        // navigate the window location to the next html adding the nameInput as query parameter to the end of the url
+    }
+}
+
+const playerNameForm = document.querySelector("#playerNameForm")
+playerNameForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    submitCheck(event);
+});
---
+++ questions.js
@@ -0,0 +1,55 @@
+//create variables for questions and answer
+let question;
+let answer;
+let username;
+
+// define username with searchParams to get nameInput from url
+// Make Hello <username> element to show on page
+
+//create function for fetch
+function getQuestion() {
+    const base_URL = "https://opentdb.com/api.php";
+    const url = `${base_URL}?amount=1&difficulty=hard`;
+    console.log(url)
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+                console.log(data)
+                //change variables for question and answer after fetch
+            question = data.results[0].question;
+            console.log(question);
+
+            answer = data.results[0].correct_answer;
+            console.log(answer)
+
+            // let correctAnswer = data.results[0].correct_answer
+            //grab from html where question is placed in the page, use div
+            let questionDiv = document.querySelector("#question")
+            questionDiv.textContent = `${question}`
+            //hide the answer button
+            const getAnswerButton = document.querySelector('#getAnswerButton');
+            getAnswerButton.style.display ="block"
+
+
+    // article.append(revealAnswerButton);
+        })
+}
+
+//action after clicking the question button
+getQuestionButton.addEventListener("click", (event)=> {
+    event.preventDefault(event);
+    //console.log('getQuestion ran')
+    getQuestion();
+
+})
+//action after clicking the answer button
+getAnswerButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(answer)
+
+    const answerDiv = document.querySelector("#answerContainer")
+    answerDiv.textContent = `${answer}`
+
+});
+
+  
