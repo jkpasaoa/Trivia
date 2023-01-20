6106d75f7118faeb5a5ca0cb5ea2b6c0e00d0ecf
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-01-20 02:57:59 -0500

    made question visibly immediate on question.html page and changed button name. reset button in process for later update

--- questions.js
+++ questions.js
@@ -6,8 +6,6 @@ let category;
 // define username with searchParams to get nameInput from url
 // Make Hello <username> element to show on page --> It is visible on index.html
 
-//show category automatically on page
-
 //create function for fetch
 function getQuestion() {
     const base_URL = "https://opentdb.com/api.php";
@@ -17,21 +15,25 @@ function getQuestion() {
         .then(response => response.json())
         .then(data => {
                 console.log(data)
-                //change variables for question and answer after fetch
+
+             //change variables for question and answer after fetch
+            //question from fetch
             question = data.results[0].question;
             console.log(question);
-
+            
+            //answer from fetch //let correctAnswer = data.results[0].correct_answer
             answer = data.results[0].correct_answer;
             console.log(answer)
 
+
+            //category from fetch
             category = data.results[0].category;
             console.log(category)
 
-            //shows category of the question
+            //shows category of the question immediately
             let categoryDiv = document.querySelector("#category")
             categoryDiv.textContent = `${category}`
 
-            // let correctAnswer = data.results[0].correct_answer
             //grab from html where question is placed in the page, use div
             let questionDiv = document.querySelector("#question")
             questionDiv.textContent = `${question}`
@@ -39,26 +41,39 @@ function getQuestion() {
             const getAnswerButton = document.querySelector('#getAnswerButton');
             getAnswerButton.style.display ="block"
 
-
     // article.append(revealAnswerButton);
         })
 }
 
-//action after clicking the question button
-getQuestionButton.addEventListener("click", (event)=> {
+    // action after clicking the question button
+    getQuestionButton.addEventListener("click", (event)=> {
     event.preventDefault(event);
     //console.log('getQuestion ran')
     getQuestion();
 
 })
-//action after clicking the answer button
-getAnswerButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log(answer)
 
-    const answerDiv = document.querySelector("#answerContainer")
-    answerDiv.textContent = `${answer}`
-
+     //action after clicking the answer button
+     getAnswerButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log(answer)
+    
+        const answerDiv = document.querySelector("#answerContainer")
+        answerDiv.textContent = `${answer}`
+    
 });
 
-  
+//question loads immediately when user first arrives on page
+question.onreadystatechange = getQuestion() 
+	if (document.readyState !== "complete") {
+		document.querySelector("body").style.visibility = "hidden";
+		document.querySelector("#question").style.visibility = "visible";
+	} else {
+		document.querySelector("#question").style.display = "none";
+		document.querySelector("body").style.visibility = "visible";
+	};
+
+    // const resetButton = document.querySelector("section");
+    // resetButton.addEventListener("click", (event) => {
+    //     event.reset();
+    // });
